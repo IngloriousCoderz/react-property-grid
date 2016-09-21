e356e022ddc66b867587ac4698d4f605de7fa090
IceOnFire <antony.mistretta@gmail.com>
2016-09-21 16:07:06 +0200

    Style minimal form and provide kitchen sink schema

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Form from 'react-json-editor'
-import logo from './logo.svg';
-import './App.css';
-import schema from './layout-schema.json'
+import './form.css'
+// import logo from './logo.svg';
+// import './App.css';
+// import schema from './layout-schema.json'
+
+const schema = {
+  title: "Contact",
+  description: "General contact info",
+  type      : "object",
+  properties: {
+    name: {
+      type: "string",
+      title: "Name"
+    },
+    age: {
+      type: "number",
+      title: "Age",
+      minimum: 0,
+      maximum: 100
+    },
+    gender: {
+      title: 'Gender',
+      description: "Your sex",
+      enum: [ "", "Male", "Female", "Apache Helicopter" ]
+    },
+    happy: {
+      title: 'Happy',
+      type: 'boolean'
+    },
+    contact: {
+      title      : "Contact details",
+      description: "How would you like to be contacted?",
+      type       : "object",
+      properties : {
+        contactType: {
+          title      : "Contact medium",
+          description: "Please pick your preferred medium",
+          enum: [ "", "Email", "Telephone", "Physical mail" ]
+        }
+      },
+      oneOf: [
+        {},
+        {
+          properties: {
+            contactType: { enum: [ "Email" ] },
+            email      : { type: "string", title: "Email address" }
+          }
+        },
+        {
+          properties: {
+            contactType: { enum: [ "Telephone" ] },
+            phoneNumber: { title: "Telephone number" }
+          }
+        },
+        {
+          properties: {
+            contactType: { enum: [ "Physical mail" ] },
+            address    : { title: "Street address" },
+            postcode   : { title: "Post or area code" },
+            state      : { title: "State or province" },
+            country    : { title: "Country" }
+          }
+        }
+      ],
+      "x-hints": { form: { selector: "contactType" } }
+    }
+  }
+}
 
 class App extends Component {
-  onSubmit() {
-    alert('hi!')
+  onSubmit(data) {
+    console.log(data)
   }
 
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <Form schema={schema} onSubmit={this.onSubmit} />
-      </div>
-    )
+    return <Form schema={ schema } onSubmit={ this.onSubmit } />
   }
 }
 
