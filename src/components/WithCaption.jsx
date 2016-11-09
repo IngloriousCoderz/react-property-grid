a83040fbdd28b86b3f12ac83f5211b8309e83518
IceOnFire <antony.mistretta@gmail.com>
2016-11-09 13:35:35 +0100

    Fix indentation of nested elements

--- src/components/WithCaption.jsx
+++ src/components/WithCaption.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-import {asciiTree} from '../utilities/path'
+import {level} from '../utilities/path'
 import {row, ellipsis, cell, buttonGroup, button} from './styles'
+
+const EXPANDER_WIDTH = 10
+const EXPANDED_ENTITY = '&dtri;'
+const COLLAPSED_ENTITY = '&rtri;'
 
 const headerRow = {
   ...row,
@@ -23,21 +27,23 @@ const fieldCell = {
 
 const paddedButtonGroup = {
   ...buttonGroup,
-  padding: 2
+  padding: 3
 }
 
 const WithCaption = ({field}) => Component => ({schema, data, title, path, required, expanded, toggleExpanded, setData, canAdd, addItem, canRemove, removeItem}) => (
   <div style={field ? row : headerRow}>
-    <div style={cell}>
-      <span dangerouslySetInnerHTML={{__html: asciiTree(path, expanded)}} onClick={toggleExpanded} />
+    <div style={{...cell, paddingLeft: cell.padding + EXPANDER_WIDTH * (level(path) + (expanded != null ? 0 : 1))}}>
+      {expanded != null ? <span dangerouslySetInnerHTML={{__html: expanded ? EXPANDED_ENTITY : COLLAPSED_ENTITY}} onClick={toggleExpanded} /> : null}
       <span style={label}>{title}</span>
       {required ? <span style={redStar}>*</span> : null}
     </div>
     <div style={field ? fieldCell : cell}>
-      <div style={field ? paddedButtonGroup : buttonGroup}>
-        {canRemove ? <div style={button} onClick={() => removeItem(path, schema)}>&ndash;</div> : null}
-        {canAdd ? <div style={button} onClick={() => addItem(path, schema)}>+</div> : null}
-      </div>
+      {canAdd || canRemove ?
+        <div style={field ? paddedButtonGroup : buttonGroup}>
+          {canRemove ? <div style={button} onClick={() => removeItem(path, schema)}>-</div> : null/*&ndash;*/}
+          {canAdd ? <div style={button} onClick={() => addItem(path, schema)}>+</div> : null}
+        </div>
+      : null}
       <div style={ellipsis}>
         <Component schema={schema} data={data} title={title} path={path} setData={setData} canAdd={canAdd} canRemove={canRemove} addItem={addItem} removeItem={removeItem} />
       </div>
--- src/utilities/path.js
+++ src/utilities/path.js
@@ -5,8 +5,3 @@ export const join = path => path.join(SEPARATOR)
 export const subpath = (path, key) => path != null ? `${path}${SEPARATOR}${key}` : key
 export const last = path => split(path).reverse()[0]
 export const level = path => split(path).length - 1
-export const asciiTree = (path, expanded) => {
-  const deep = level(path)
-  const spacing = deep ? `${'&ensp;'.repeat(2 * deep)}` : ''//&dlcorn;
-  return expanded != null ? `${spacing}${expanded ? '&dtri;' : '&rtri;'}` : spacing
-}
